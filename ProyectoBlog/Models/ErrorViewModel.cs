e4e44b39c12c1de49b7120e78ae64890c49df64f
adrianvillagomez <adrian132892@gmail.com>
2023-03-25 16:21:59 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoBlog.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
